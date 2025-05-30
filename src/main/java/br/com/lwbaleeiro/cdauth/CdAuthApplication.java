d4a50344d798a45bca3bb0e2beea4a611fc6385c
Luan Baleeiro <dev.baleeiro@gmail.com>
2025-05-30 00:22:48 -0300

    [feat] Initial configuration and readme

---
+++ src/main/java/br/com/lwbaleeiro/cdauth/CdAuthApplication.java
@@ -0,0 +1,13 @@
+package br.com.lwbaleeiro.cdauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lwbaleeiro/cdauth/CdAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lwbaleeiro.cdauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
